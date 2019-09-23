222b78aca6b73c653b6994b77976ff0744815f28
zhouxin <zhouxin@rd.netease.com>
2019-09-23 13:36:44 +0800

    MOD

--- tryCatchTest/src/com/zhouxin/Main.java
+++ tryCatchTest/src/com/zhouxin/Main.java
@@ -4,5 +4,42 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        try {
+            try2();
+            try3();
+        } catch (Exception e) {
+            e.printStackTrace();
+            //System.out.println(e);
+        }
+
+        /*try {
+            try3();
+        } catch (Exception e) {
+//            e.printStackTrace();
+            System.out.println(e);
+
+        }*/
+    }
+    public static String try1(){
+        try {
+            System.out.println(1/0);
+            return "success";
+        } catch (Exception e) {
+            System.out.println(e);
+            return "failed";
+        }
+    }
+
+    public static void try2() {
+        try {
+            int x = 1 / 0;
+            System.out.println(x);
+        } catch (Exception e) {
+            System.out.println("try2 : " + e);
+        }
+    }
+
+    public static void try3() throws Exception {
+        throw new Exception();
     }
 }
